84610d8f1edd0bc86e9b8fc9efd6a0dc62c849d8
Su HeRui <90187998+Henry-NS-2021@users.noreply.github.com>
2021-11-16 17:06:04 +0100

    LoginSystem
    Beginning of building the login system

---
+++ LoginUser/components/db_connect.php
@@ -0,0 +1,22 @@
+<?php
+
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "crud_login";
+
+
+    $connect = new mysqli($hostname, $username, $password, $dbname);
+
+    
+    if(!$connect){
+        die("Connection failed:" . mysqli_connect_error());
+    } else {
+        echo "<h1>DB_CONNECT: Yes. We made it (connected)!</h1>";
+    }
+
+
+
+
+
+?>
